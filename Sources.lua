25c445340cbf4c027a2127dcbcb6364c3c2ffb4a
Peter Bjerg Mogensen <pbm@impact.dk>
2024-12-12 20:33:10 +0100

    flipped sources so the value is the name of the attack

--- Sources.lua
+++ Sources.lua
@@ -1,35 +1,33 @@
-local L = {}
-
 L.WARRIOR = {
-	Damage = { 
-		78,    -- Heroic Strike
-		7373,  -- Hamstring
-		7384,  -- Overpower
-		6572,  -- Revenge
-		694,   -- Mocking Blow
-		5308,  -- Execute
-		1464,  -- Slam
-		6552,  -- Pummel
-		23881, -- Bloodthirst
-		12294, -- Mortal Strike
-		20240  -- Retaliation
+	Damage = {
+		"Heroic Strike", -- 78
+		"Hamstring",     -- 7373
+		"Overpower",     -- 7384
+		"Revenge",       -- 6572
+		"Mocking Blow",  -- 694
+		"Execute",       -- 5308
+		"Slam",          -- 1464
+		"Pummel",        -- 6552
+		"Bloodthirst",   -- 23881
+		"Mortal Strike", -- 12294
+		"Retaliation",   -- 20240
 	},
 	AreaEffect = {
-		845,  -- Cleave
-		1680  -- Whirlwind
+		"Cleave",    -- 845
+		"Whirlwind", -- 1680
 	},
 	Aura = {
-		676,  -- Disarm
-		772,  -- Rend
-		7386  -- Sunder Armor
+		"Disarm",       -- 676,
+		"Rend",         -- 772,
+		"Sunder Armor", -- 7386
 	}
 }
 
 L.PALADIN = {
 	Damage = {
-		21084, -- Seal of Righteousness
-		20375, -- Seal of Command
-		20467  -- Judgement of Command
+		"Seal of Righteousness", -- 21084
+		"Seal of Command",       -- 20375
+		"Judgement of Command",  -- 20467
 	},
 	Aura = {
 	}
@@ -37,27 +35,27 @@ L.PALADIN = {
 
 L.ROGUE = {
 	Damage = {
-		53,    -- Backstab
-		1776,  -- Gouge
-		1766,  -- Kick
-		1752,  -- Sinister Strike
-		2098,  -- Eviscerate
-		8676,  -- Ambush
-		14251, -- Riposte
-		16511  -- Hemorrhage	
+		"Backstab",        -- 53,
+		"Gouge",           -- 1776,
+		"Kick",            -- 1766,
+		"Sinister Strike", -- 1752,
+		"Eviscerate",      -- 2098,
+		"Ambush",          -- 8676,
+		"Riposte",         -- 14251,
+		"Hemorrhage",      -- 16511
 	},
 	Aura = {
-		1833, -- Cheap Shot
-		408,  -- Kidney Shot
-		703,  -- Garrote
-		1943, -- Rupture
-		6770  -- Sap
+		"Cheap Shot",  -- 1833
+		"Kidney Shot", -- 408
+		"Garrote",     -- 703
+		"Rupture",     -- 1943
+		"Sap",         -- 6770
 	}
 }
 
 L.SHAMAN = {
 	Damage = {
-		17364,   -- Stormstrike
+		"Stormstrike", -- 17364
 	},
 	Aura = {
 	}
