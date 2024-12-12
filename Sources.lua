7cbaf233029f0346b96fc6aa130b287b5d895cfa
Peter Bjerg Mogensen <pbm@impact.dk>
2024-12-12 22:30:31 +0100

    - Sources now use the spell name as spellID is not available on 1.12 via command - Procs now include the spell name as the spell name is not available on 1.12 via command

--- Sources.lua
+++ Sources.lua
@@ -1,33 +1,33 @@
 L.WARRIOR = {
 	Damage = {
-		"Heroic Strike", -- 78
-		"Hamstring",     -- 7373
-		"Overpower",     -- 7384
-		"Revenge",       -- 6572
-		"Mocking Blow",  -- 694
-		"Execute",       -- 5308
-		"Slam",          -- 1464
-		"Pummel",        -- 6552
-		"Bloodthirst",   -- 23881
-		"Mortal Strike", -- 12294
-		"Retaliation",   -- 20240
+		["Heroic Strike"] =  78,
+		["Hamstring"]     =  7373,
+		["Overpower"]     =  7384,
+		["Revenge"]       =  6572,
+		["Mocking Blow"]  =  694,
+		["Execute"]       =  5308,
+		["Slam"]          =  1464,
+		["Pummel"]        =  6552,
+		["Bloodthirst"]   =  23881,
+		["Mortal Strike"] =  12294,
+		["Retaliation"]   =  20240,
 	},
 	AreaEffect = {
-		"Cleave",    -- 845
-		"Whirlwind", -- 1680
+		["Cleave"]    = 845,
+		["Whirlwind"] = 1680,
 	},
 	Aura = {
-		"Disarm",       -- 676,
-		"Rend",         -- 772,
-		"Sunder Armor", -- 7386
+		["Disarm"]       = 676,
+		["Rend"]         = 772,
+		["Sunder Armor"] = 7386,
 	}
 }
 
 L.PALADIN = {
 	Damage = {
-		"Seal of Righteousness", -- 21084
-		"Seal of Command",       -- 20375
-		"Judgement of Command",  -- 20467
+		["Seal of Righteousness"] = 21084,
+		["Seal of Command"]       = 20375,
+		["Judgement of Command"]  = 20467,
 	},
 	Aura = {
 	}
@@ -35,27 +35,27 @@ L.PALADIN = {
 
 L.ROGUE = {
 	Damage = {
-		"Backstab",        -- 53,
-		"Gouge",           -- 1776,
-		"Kick",            -- 1766,
-		"Sinister Strike", -- 1752,
-		"Eviscerate",      -- 2098,
-		"Ambush",          -- 8676,
-		"Riposte",         -- 14251,
-		"Hemorrhage",      -- 16511
+		["Backstab"]        = 53,
+		["Gouge"]           = 1776,
+		["Kick"]            = 1766,
+		["Sinister Strike"] = 1752,
+		["Eviscerate"]      = 2098,
+		["Ambush"]          = 8676,
+		["Riposte"]         = 14251,
+		["Hemorrhage"]      = 16511,
 	},
 	Aura = {
-		"Cheap Shot",  -- 1833
-		"Kidney Shot", -- 408
-		"Garrote",     -- 703
-		"Rupture",     -- 1943
-		"Sap",         -- 6770
+		["Cheap Shot"]  = 1833,
+		["Kidney Shot"] = 408,
+		["Garrote"]     = 703,
+		["Rupture"]     = 1943,
+		["Sap"]         = 6770,
 	}
 }
 
 L.SHAMAN = {
 	Damage = {
-		"Stormstrike", -- 17364
+		["Stormstrike"] = 17364
 	},
 	Aura = {
 	}
